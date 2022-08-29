c44362555f3a43cc981704b1dc7e887846d00cb8
RuslanPerian <107149196+RuslanPerian@users.noreply.github.com>
2022-08-29 21:49:34 +0300

    Update vh.js

--- js/vh.js
+++ js/vh.js
@@ -1,7 +1,10 @@
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
+let hv = () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+hv();
+
+window.addEventListener('resize', () => {
+    hv();
 });
